17a15a91c6156baaa4abe4412ec005af49c78b5e
ChienliMa <maqianlie@gmail.com>
2016-09-29 17:09:42 -0400

    Add click function to web-site new. However, I can share dat between different view.

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -5,10 +5,16 @@
 (function(angular) {
     'use strict';
     angular.module('myApp', []).controller('WebsiteController', function($scope) {
-        $scope.websites= [{"name":"spotify"},
-                            {"name":"medude"},
-                            {"name":"dwidder"}];
 
 
+        $scope.websites= [{"name":"spotify", "desc":""},
+                            {"name":"medude", "desc":""},
+                            {"name":"dwidder", "desc":""}];
+
+        $scope.addWebsite = function(website){
+            console.log($scope.websites);
+            $scope.websites.push(website);
+            console.log($scope.websites);
+        };
     });
 })(window.angular);
