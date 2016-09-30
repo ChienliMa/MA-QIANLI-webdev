01f11220f39995eedf970d792fae0330e030af68
ChienliMa <maqianlie@gmail.com>
2016-09-29 21:41:04 -0400

    complete all page and widget-list. need to refine widget-list's style

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -4,17 +4,31 @@
 
 (function(angular) {
     'use strict';
-    angular.module('myApp', []).controller('WebsiteController', function($scope) {
+    var myApp = angular.module('myApp', []);
 
+    myApp.filter("trustUrl", ['$sce', function ($sce) {
+        return function (recordingUrl) {
+            return $sce.trustAsResourceUrl(recordingUrl);
+        };
+    }]);
 
+    myApp.controller('WebsiteController', function($scope) {
         $scope.websites= [{"name":"spotify", "desc":""},
                             {"name":"medude", "desc":""},
                             {"name":"dwidder", "desc":""}];
 
-        $scope.addWebsite = function(website){
-            console.log($scope.websites);
-            $scope.websites.push(website);
-            console.log($scope.websites);
-        };
+    });
+
+    myApp.controller('PageController', function($scope) {
+        $scope.pages= [{"name":"Onepage", "title":""},
+            {"name":"Mypage", "title":""},
+            {"name":"Yourpage", "title":""}];
+    });
+
+    myApp.controller('WidgetController', function($scope) {
+        $scope.TYPE = {HEADING:0, IMAGE:1, YOUTUBE:2}
+        $scope.widgets = [{"type":$scope.TYPE.HEADING, "name":"heading", "content":"HELLO WORLD!"},
+                         {"type":$scope.TYPE.IMAGE, "name":"hello world", "content":"http://browningit.com/wp-content/uploads/2015/09/helloworld.gif"},
+                         {"type":$scope.TYPE.YOUTUBE, "name":"PPAP", "content":"https://www.youtube.com/embed/yygpgCFJFnA?html5=1"}];
     });
 })(window.angular);
