00d7fda89719f5caf0f447265268551e919440f0
ChienliMa <maqianlie@gmail.com>
2016-10-10 21:03:17 -0400

    1.Rename all view file name 2.delete all view header and left only body 3.add angular-css to introduce on demand css feature

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -2,25 +2,25 @@
  * Created by ChienliMa on 10/9/16.
  */
 (function() {
-    angular.module("myApp", ["ngRoute"]).config(Config);
+    angular.module("myApp", ["ngRoute", 'angularCSS']).config(Config);
 
     var routes = [
-        ["/login",  "views/user/login.view.client.html", ""],
-        ["/default",    "login.view.client.html", ""],
-        ["/register	",  "register.view.client.html", ""],
-        ["/user/:uid",  "profile.view.client.html", ""],
+        ["/login",  "user/login.view.client.html", "default-style.css"],
+        ["/default",    "user/login.view.client.html", "default-style.css"],
+        ["/register",  "user/register.view.client.html", "default-style.css"],
+        ["/user/:uid",  "user/profile.view.client.html", "website-style.css"],
 
-        ["/user/:uid/website",  "website-list.view.client.html", "css/website-style.css"],
-        ["/user/:uid/website/new",  "website-new.view.client.html", "css/website-style.css"],
-        ["/user/:uid/website/:wid", "website-edit.view.client.html", "css/website-style.css"],
+        ["/user/:uid/website",  "website/website-list.view.client.html", "website-style.css"],
+        ["/user/:uid/website/new",  "website/website-new.view.client.html", "website-style.css"],
+        ["/user/:uid/website/:wid", "website/website-edit.view.client.html", "website-style.css"],
 
-        ["/user/:uid/website/:wid/page",    "page-list.view.client.html", "css/page-style.css"],
-        ["/user/:uid/website/:wid/page/new",    "page-new.view.client.html", "css/page-style.css"],
-        ["/user/:uid/website/:wid/page/:pid",   "page-edit.view.client.html", "css/page-style.css"],
+        ["/user/:uid/website/:wid/page",    "page/page-list.view.client.html", "page-style.css"],
+        ["/user/:uid/website/:wid/page/new",    "page/page-new.view.client.html", "page-style.css"],
+        ["/user/:uid/website/:wid/page/:pid",   "page/page-edit.view.client.html", "page-style.css"],
 
-        ["/user/:uid/website/:wid/page/:pid/widget",    "widget-list.view.client.html", "css/widget-style.css"],
-        ["/user/:uid/website/:wid/page/:pid/widget/new",    "widget-chooser.view.client.html", "css/widget-style.css"],
-        ["/user/:uid/website/:wid/page/:pid/widget/:wgid",  "widget-edit.view.client.html", "css/widget-style.css"]
+        ["/user/:uid/website/:wid/page/:pid/widget",    "widget/widget-list.view.client.html", "widget-style.css"],
+        ["/user/:uid/website/:wid/page/:pid/widget/new",    "widget/widget-chooser.view.client.html", "widget-style.css"],
+        ["/user/:uid/website/:wid/page/:pid/widget/:wgid",  "widget/widget-edit.view.client.html", "widget-style.css"]
     ];
 
     function Config($routeProvider) {
@@ -31,7 +31,9 @@
             view = routes[i][1];
             css = routes[i][2];
 
-            $routeProvider.when(uri, {templateUrl: view, css: [css],});
+            $routeProvider.when(uri,
+                {templateUrl: "views/" + view,
+                    css: "css/" + css});
         }
         $routeProvider.otherwise("/login");
     }
