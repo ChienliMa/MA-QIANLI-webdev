161f8e0a2e3bd1e253c2ee36839c47507c233fb7
ChienliMa <maqianlie@gmail.com>
2016-10-19 21:42:22 -0400

    service and controller of pages work.

--- public/assignment/views/page/page.controller.client.js
+++ public/assignment/views/page/page.controller.client.js
@@ -7,17 +7,42 @@
     var myApp = angular.module('myApp');
 
     myApp.controller("PageListController", PageListController);
-    var PageListController = function($scope, $routeParams){
+    function PageListController($scope, $routeParams, PageService){
+        this.uid = $routeParams.uid.toString();
+        this.wid = $routeParams.wid.toString();
 
+        this.pages = PageService.findPageByWebsiteId(this.wid);
     };
 
     myApp.controller("NewPageController", NewPageController);
-    var NewPageController = function($scope, $routeParams){
+    function NewPageController($scope, $routeParams, $location, PageService){
+        this.uid = $routeParams.uid.toString();
+        this.wid = $routeParams.wid.toString();
 
+        this.page = {name: "", websiteId: "", description: "", title: ""};
+
+        this.newPage = function(){
+            PageService.createPage(this.wid, angular.copy(this.page));
+            $location.path("/user/" + this.uid + "/website/" + this.wid + "/page/");
+        };
     };
 
     myApp.controller("EditPageController", EditPageController);
-    var EditPageController = function($scope, $routeParams){
+    function EditPageController($scope, $routeParams, $location, PageService){
+        this.uid = $routeParams.uid.toString();
+        this.wid = $routeParams.wid.toString();
+        this.pid = $routeParams.pid.toString();
 
+        this.page = PageService.findPageById(this.pid);
+
+        this.update = function(){
+            PageService.updatePage(this.pid, angular.copy(this.page));
+            $location.path("/user/" + this.uid + "/website/" + this.wid + "/page/");
+        };
+
+        this.delete = function(){
+            PageService.deletePage(this.pid);
+            $location.path("/user/" + this.uid + "/website/" + this.wid + "/page/");
+        };
     };
 })(window.angular);
