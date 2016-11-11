918531748bdce2341b6fdb952335b5b6b70210c7
ChienliMa <maqianlie@gmail.com>
2016-11-11 16:26:44 -0500

    Add personal page

--- public/assignment/views/widget/widget.controller.client.js
+++ public/assignment/views/widget/widget.controller.client.js
@@ -113,5 +113,16 @@
                     model.widget.width = "100%";
                 });
         };
+
+        model.searchPhotos = function(searchTerm) {
+            FlickrService
+                .searchPhotos(searchTerm)
+                .then(function(response) {
+                    data = response.data.replace("jsonFlickrApi(","");
+                    data = data.substring(0,data.length - 1);
+                    data = JSON.parse(data);
+                    model.photos = data.photos;
+                });
+        };
     }
 })(window.angular);
