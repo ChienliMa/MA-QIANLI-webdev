62f2c7b4499b91dad95d45a15f814aa5a17182bc
ChienliMa <maqianlie@gmail.com>
2016-11-27 23:35:45 -0500

    ad log to user

--- public/assignment/server/model/user.model.server.js
+++ public/assignment/server/model/user.model.server.js
@@ -20,6 +20,8 @@ module.exports = function(model){
         },
 
         updateUser : function(userId, user){
+            console.log(user);
+            console.log("LOL");
             return User.findOneAndUpdate({_id : userId}, {$set : user}, {upsert: true, new : true});
         },
 
