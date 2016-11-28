4b73fe696fe70afe06941c2715e2c5ab9c1e010d
ChienliMa <maqianlie@gmail.com>
2016-11-27 22:35:05 -0500

    don't automatically create user

--- public/assignment/server-app.js
+++ public/assignment/server-app.js
@@ -2,8 +2,6 @@ module.exports = function(app) {
     // models
     var model = require("./server/model/models.server")();
 
-    model.Users.createUser({username:"aaa",password:"aaa"});
-
     // service
     require("./server/services/user.service.server.js")(app, model);
     require("./server/services/website.service.server.js")(app, model);
